a4072101f47839df63255dc85a17ff7d1573965a
Juan Pablo Pilorget <jpilorget@gmail.com>
2020-09-17 22:02:55 -0300

    Actualizo main.ts con Main, getters y setters

--- src/front/ts/main.ts
+++ src/front/ts/main.ts
@@ -6,6 +6,79 @@
  * Brief: Main frontend file (where the logic is)
 =============================================================================*/
 
+class Main {
+    main():void {
+        console.log("estoy en main()");
+
+        let usuarios:Array<User>;
+        usuarios = new Array<User>();
+        usuarios.push(new User(15, "Juan", "juan@email.com"))
+        usuarios.push(new User(24, "Pedro", "pedro@email.com"))
+        usuarios.push(new User(39, "Maria", "maria@email.com"))
+
+        for (let i in usuarios) 
+        {
+            usuarios[i].printInfo ();
+        }
+
+    }
+
+}
+
+
+class User {
+    private _id:number;
+    private _name:string;
+    private _email:string;
+    private _isLogged:boolean;
+
+    constructor (id:number, name:string, email:string) {
+        this._id = id;
+        this._name = name;
+        this._email = email;
+    }
+
+    set id(id:number)
+    {
+        this._id = id;
+
+    }
+
+    get id():number {
+        return this._id
+    }
+
+    set name(name:string)
+    {
+        this._name = name;
+
+    }
+
+    get name():string {
+        return this._name
+    }
+
+    set email(email:string)
+    {
+        this._email = email;
+
+    }
+
+    get email():string {
+        return this._email
+    }
+
+    printInfo():void {
+        console.log("id = " + this._id +  ", name = " + this._name + ", email = " + this._email)
+    }
+}
+
+
+window.onload = function () {
+    let m:Main = new Main();
+    m.main ()
+}
+
 //=======[ Settings, Imports & Data ]==========================================
 
 let user = "TypesScript Users!";
@@ -16,6 +89,9 @@ function greeter(person) {
     return "Hello, " + person;
  }
  
- document.body.innerHTML = greeter(user);
+// document.body.innerHTML = greeter(user);
+
+console.log("Hola mundo!");
+
 
 //=======[ End of file ]=======================================================
